aa67a09eb20c54b5103c5463542ab829a280b872
Vladimir Valeyev <ValV@users.noreply.github.com>
2019-05-22 22:57:57 +0600

    Add performance reporting. Replace ProgramOptions with CommandLineParser

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,50 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/imgproc/types_c.h"
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/videoio/videoio.hpp"
 
-#include <unistd.h>
-#include <getopt.h>
+#include <sys/stat.h>
 #include <iostream>
 #include <string>
-
-#include "options.hpp"
 
 using namespace cv;
 using namespace std;
 
+inline bool exists(const std::string &path) {
+  struct stat buffer;
+  return (stat (path.c_str(), &buffer) == 0);
+};
+
+const char *keys =
+  "{ help h | | Print help message. }"
+  "{ face f |/usr/share/opencv4/haarcascades"
+  "/haarcascade_frontalface_default.xml|"
+  "             Path to pretrained face detect model (.xml). }"
+  "{ eyes e |/usr/share/opencv4/haarcascades"
+  "/haarcascade_eye.xml|"
+  "             Path to pretrained eye detect model (.xml). }"
+  ;
+
+
 int main(int argc, char *argv[]) {
   // Configure program options
-  ProgramOptions *options = new ProgramOptions(argv[0]);
-  if (options == NULL) {
-    cout << " Failed to initialize program options. Terminating..." << endl;
-    exit(-1);
-  } else {
-    options->ParseOptions(argc, argv);
-    cout << "Options/faces: " << options->haar_faces() << endl;
-    cout << "Options/eyes: " << options->haar_eyes() << endl;
-    cout << "Options/data: " << options->user_data() << endl;
-    cout << endl;
+  CommandLineParser parser(argc, argv, keys);
+  parser.about("Face detection OpenCV application");
+  if (parser.has("help")) {
+    parser.printMessage();
+    return 0;
   }
 
-  // Create cascade classifier: default face Haar Cascade resides in
-  // /usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml
-  CascadeClassifier haar_cascade;
-  haar_cascade.load(options->haar_faces());
-
-  // Create extra cascade for eyes
-  CascadeClassifier haar_extra_eyes;
-  haar_extra_eyes.load(options->haar_eyes());
+  try {
+    CV_Assert(exists(parser.get<string>("face")));
+    CV_Assert(exists(parser.get<string>("eyes")));
+  } catch(...) {
+    parser.printMessage();
+    cout << "Error: wrong command line or missing files!" << endl;
+    cout << parser.get<string>("face") << endl;
+    return 1;
+  }
 
   // Open video capture device
   VideoCapture cap(0); // 0 - default video capture device
@@ -44,60 +52,78 @@ int main(int argc, char *argv[]) {
     cerr << "Capture device ID 0 cannot be opened." << endl;
   }
 
+  // Create Haar face cascade classifier
+  CascadeClassifier haar_face;
+  haar_face.load(parser.get<string>("face"));
+
+  // Create extra cascade for eyes
+  CascadeClassifier haar_eyes;
+  haar_eyes.load(parser.get<string>("eyes"));
+
   // Process video input per frame
   Mat frame;
+  double frames_total = 0, frames_face = 0, frames_eyes = 0;
+  double time_face = 0, time_eyes = 0;
+  clock_t time_start = 0;
   while (true) {
     // Get current frame, convert it to gray to detect a face
     cap >> frame;
+    frames_total ++;
     Mat original = frame.clone();
     Mat gray;
-    cvtColor(original, gray, CV_BGR2GRAY); // convert the frame
+    cvtColor(original, gray, COLOR_BGR2GRAY);
     vector<Rect_<int>> faces;
     double face_side = sqrt(original.cols * original.rows);
     // Detect a face (5..55% of a frame)
-    haar_cascade.detectMultiScale(gray, faces, 1.2, 3,
+    time_start = clock();
+    haar_face.detectMultiScale(gray, faces, 1.2, 4,
         CASCADE_SCALE_IMAGE,
         Size(face_side * 0.05, face_side * 0.05),
         Size(face_side * 0.55, face_side * 0.55));
+    time_face = (double) (clock() - time_start) / CLOCKS_PER_SEC;
 
     // Now faces variable holds rectangles for all detected faces
-    // in the current frame. Annotate every face in the frame
+    // in the current frame. Annotate a face in the frame
     if (faces.size() == 1) {
+      frames_face ++;
       Rect face_rect = faces[0];
       vector<Rect_<int>> eyes;
       // Detect eyes (10..30% of a face)
-      haar_extra_eyes.detectMultiScale(Mat(gray, face_rect), eyes, 1.2, 3,
+      time_start = clock();
+      haar_eyes.detectMultiScale(Mat(gray, face_rect), eyes, 1.2, 3,
           CASCADE_SCALE_IMAGE,
           Size(face_rect.width * 0.1, face_rect.height * 0.1),
           Size(face_rect.width * 0.3, face_rect.height * 0.3));
+      time_eyes = (double) (clock() - time_start) / CLOCKS_PER_SEC;
       if (eyes.size() == 2) { // early reject false face
-        Rect eyes_line = eyes[0] | eyes[1];
-        if (eyes_line.width > 2 * eyes_line.height) { // false eyes
-          // Highlight the face with a rectangle and a text in the frame
-          rectangle(original, face_rect, CV_RGB(0, 255, 0), 1);
-          rectangle(original, eyes_line + face_rect.tl(), CV_RGB(0, 0, 255), 1);
-
-          string box_text = format("Face: %dx%d px",
-              face_rect.width, face_rect.height);
-
-          int pos_x = face_rect.tl().x + 2;
-          int pos_y = face_rect.tl().y + 14;
-
-          putText(original, box_text, Point(pos_x, pos_y),
-              FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0, 255, 0), 2.0);
-          // Display the frame with face detected
-          imshow("facelock", (original));
+        Rect eyes_rect = eyes[0] | eyes[1];
+        if (eyes_rect.width > 2 * eyes_rect.height) {
+          frames_eyes ++;
+          rectangle(original, eyes_rect + face_rect.tl(),
+              CV_RGB(60, 179, 113));
         }
       }
+      // Highlight the face with a rectangle
+      rectangle(original, face_rect, CV_RGB(46, 139, 87));
     }
 
+    // Display detector performance (accuracy + time)
+    string box_text = format("Accuracy: %3.1f%% (%3.1f%%), "
+        "time: %.3fs (%.3fs)", frames_eyes / frames_total * 100,
+        frames_face / frames_total * 100, time_eyes + time_face,
+        time_face);
+    putText(original, box_text, Point(2, 14),
+        FONT_HERSHEY_PLAIN, 1.0, CV_RGB(199, 21, 133), 2.0);
+
+    // Display the frame with face detected
+    imshow("facelock", (original));
+
     // Wait and catch keypress
-    char key = (char) waitKey(80);
+    char key = (char) waitKey(250);
     if (key == 27) break;
   }
 
   destroyAllWindows();
-  delete options;
   return 0;
 }
 
